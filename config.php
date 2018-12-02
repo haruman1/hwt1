96e32bc7e92402ee13f41be77d92e798f1a3a679
haruman1 <30350640+haruman1@users.noreply.github.com>
2018-12-02 09:33:34 +0700

    Update config.php

--- config.php
+++ config.php
@@ -1,7 +1,7 @@
 <?php
 $config = [
-"imei" => "xxxxxxxx",
-"sign" => "xxxxxxxx",
-"token" => "xxxxxxxx",
-"uuid" => "xxxxxxxx"
+"imei" => "358167070147132",
+"sign" => "f91ce930eed50a5866b9dc56b44dfd9b3e8e2fbc",
+"token" => "36b1af9a37084f564c69178fe4038d89",
+"uuid" => "f609367d-f04d-4225-8888-6d7554b5ce75"
 ];
